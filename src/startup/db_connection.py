0778c6420105a3c32967d39c11244e91980faa08
Luis Guzman <luis.dev@lmaero.pro>
2023-04-06 08:46:27 -0500

    Add db_connection.py module

---
+++ src/startup/db_connection.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from src.startup.app_env import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+
+DATABASE_URL = (
+    f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+database = SQLAlchemy()
+
+
+def init_db(app):
+    database.init_app(app)
+    database.create_all()
