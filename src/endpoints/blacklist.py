1c884df085dc4b756f9a82a31cf3559544d43082
Alonso Cantu <a.cantu@uniandes.edu.co>
2023-04-13 17:43:28 -0600

    FEAT: Get the information in the POST from the body and modify the GET logic

--- src/endpoints/blacklist.py
+++ src/endpoints/blacklist.py
@@ -1,29 +1,29 @@
 import json
 import os
-
-from datetime import datetime
-
 import jwt
-
+from datetime import datetime
 from src.models.blacklisted import Blacklisted
 from src.schemas.blacklisted import BlacklistedSchema
+
 def post_add_email_to_blacklist(db, request):
     try:
+        data = request.get_json()
+
         bearer = request.headers.get('Authorization')
         if bearer is None or bearer == '':
             return {"msg": "Authorization header is not in the headers or bearer value is wrong"}, 400
         if len(bearer.split()) < 2:
             return {"msg": "Token is not in the headers"}, 400
 
-        email = request.args.get("email")
+        email = data["email"]
         if str(email) == '':
             return {"msg": "The email is missing, please provide a valid email"}, 400
 
-        app_uuid = request.args.get("app_uuid")
+        app_uuid = data["app_uuid"]
         if str(app_uuid) == '':
             return {"msg": "The app_uuid is missing, please provide a valid app id"}, 400
 
-        blocked_reason = request.args.get("blocked_reason")
+        blocked_reason = data["blocked_reason"]
         if len(blocked_reason) > 255:
             return {"msg": "The block reason has to be less than 255 characters"}, 400
 
@@ -50,15 +50,18 @@ def blackmail_info_get(db, request):
         bearer = request.headers.get('Authorization')
         if bearer is None or bearer == '':
             return {"msg": "Authorization header is not in the headers or bearer value is wrong"}, 400
-        if len(bearer.split()) < 2:
-            return {"msg": "Token is not in the headers"}, 400
-        token = bearer.split()[1]
+
+        email = request.args.get("email")
+        if str(email) == '':
+            return {"msg": "The email is missing, please provide a valid email"}, 400
+
         try:
-            data_decoded = jwt.decode(token, os.environ['SECRET_KEY'], algorithms='HS256')
+            blackmail = db.session.query(Blacklisted).filter_by(email=email).first()
+            info = blackmail.__dict__
+            return {"is_in_blacklist": 1, "cause": str(info["blocked_reason"])}, 200
         except Exception as e:
-            return {"msg": "Token is not valid or has already expired"}, 401
-        blackmail = db.session.query(Blacklisted).filter_by(id=data_decoded["id"]).first()
-        return {"id": int(blackmail.id), "email": str(blackmail.email), "cause": str(blackmail.cause)}, 200
+            return {"is_in_blacklist": 0, "cause": ""}, 200
+
     except Exception as e:
         return {"msg": str(e)}, 500
 
