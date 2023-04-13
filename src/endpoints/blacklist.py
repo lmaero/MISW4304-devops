a0a9d58cfcec311fba2db6345bcbcd2be3d4d88d
Alonso Cantu <a.cantu@uniandes.edu.co>
2023-04-13 17:01:16 -0600

    FEAT: Validations added for each field

--- src/endpoints/blacklist.py
+++ src/endpoints/blacklist.py
@@ -9,9 +9,24 @@ from src.models.blacklisted import Blacklisted
 from src.schemas.blacklisted import BlacklistedSchema
 def post_add_email_to_blacklist(db, request):
     try:
+        bearer = request.headers.get('Authorization')
+        if bearer is None or bearer == '':
+            return {"msg": "Authorization header is not in the headers or bearer value is wrong"}, 400
+        if len(bearer.split()) < 2:
+            return {"msg": "Token is not in the headers"}, 400
+
         email = request.args.get("email")
+        if str(email) == '':
+            return {"msg": "The email is missing, please provide a valid email"}, 400
+
         app_uuid = request.args.get("app_uuid")
+        if str(app_uuid) == '':
+            return {"msg": "The app_uuid is missing, please provide a valid app id"}, 400
+
         blocked_reason = request.args.get("blocked_reason")
+        if len(blocked_reason) > 255:
+            return {"msg": "The block reason has to be less than 255 characters"}, 400
+
         ip_address = str(request.remote_addr)
 
         new_blacklisted = Blacklisted(
